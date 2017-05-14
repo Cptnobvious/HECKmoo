20c2e4cc14f1773ef1504f293e0cba83c94b6bc7
Cptnobvious <tylercrawford2@gmail.com>
2017-05-14 04:38:38 -0500

    chatnet is working

--- src/chat/ChatChannel.java
+++ src/chat/ChatChannel.java
@@ -1,5 +1,39 @@
 package chat;
 
-public class ChatChannel {
+import playermanager.Player;
+import utility.ColorStrings;
+import utility.StringUtility;
+import verb.Verb;
 
+public abstract class ChatChannel extends Verb{
+
+	protected String channelName = null;
+	protected String channelNameColor = "$WHITE";
+	
+	abstract public boolean setChannelName();
+	abstract public boolean setChannelNameColor();
+	
+	public ChatChannel(){
+		super();
+		setChannelName();
+		setChannelNameColor();
+		setAlias();
+		ChatMaster.addChannel(this);
+	}
+	
+	public boolean run(Player ply, String str){
+		if (channelName == null){
+			return false;
+		}
+		
+		String chan = ColorStrings.getColoredText(ColorStrings.getColorIntByString(channelNameColor), ColorStrings.getBoldText("[" + channelName + "]") );
+		
+		ChatMaster.sendChat(new ChatMessage(chan, ply.getActor().getName(), StringUtility.getStringAfterFirst(str)));
+		return true;
+	}
+	
+	public String getName(){
+		return this.channelName;
+	}
+	
 }
--- src/chat/ChatMaster.java
+++ src/chat/ChatMaster.java
@@ -1,7 +1,37 @@
 package chat;
 
+import java.util.ArrayList;
+
+import playermanager.Player;
+import playermanager.PlayerController;
+
 //Decides where to send things and when
 
 public class ChatMaster {
 
+	private static ArrayList<ChatChannel> channels = new ArrayList<ChatChannel>();
+	
+	public static boolean addChannel(ChatChannel chan){
+		for (int i = 0; i < channels.size(); i++){
+			if (channels.get(i).getName().equals(chan.getName())){
+				return false;
+			}
+		}
+		channels.add(chan);
+		return true;
+	}
+	
+	public static boolean sendChat(ChatMessage chat){
+		//TODO: this sends to all
+		ArrayList<Player> players = PlayerController.getPlayersListCopy();
+		ArrayList<Player> subscribed = new ArrayList<Player>();
+		
+		subscribed = players;
+		
+		for (int i = 0; i < subscribed.size(); i++){
+			subscribed.get(i).sendMessageToClient(chat.getMessage());
+		}
+		return true;
+	}
+	
 }
