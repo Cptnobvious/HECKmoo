00ba562f7b758e87d9e164a6ef44c59999d139c0
Cptnobvious <tylercrawford2@gmail.com>
2017-05-16 17:11:20 -0500

    updated login menu class

--- src/playermanager/LogIn.java
+++ src/playermanager/LogIn.java
@@ -1,11 +1,31 @@
 package playermanager;
 
+import java.util.ArrayList;
+
 import utility.ColorStrings;
+import utility.RandomUtility;
 
 public class LogIn {
+	
+	private static ArrayList<String> quotes = new ArrayList<String>();
 
 	public static String getLoginScreen(){
-		return ColorStrings.replaceTags("$[fGREEN]Enter a username.");
+		String loginMenu = "";
+		loginMenu = loginMenu + getQuote() + "\n";
+		loginMenu = loginMenu + ColorStrings.replaceTags("$[fGREEN]Enter a username.");
+		return loginMenu;
+	}
+	
+	public static boolean setupLoginQuotes(ArrayList<String> quoteslist){
+		quotes = quoteslist;
+		return true;
+	}
+	
+	public static String getQuote(){
+		if (quotes.size() == 0){
+			return "";
+		}
+		return quotes.get(RandomUtility.getRandomNumberExclusive(0, quotes.size()));
 	}
 	
 }
